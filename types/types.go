282e9cd59095f8f9e55b1be60dcb92428c1de58e
why <why@ipfs.io>
2022-07-27 10:00:55 -0700

    some more types

--- types/types.go
+++ types/types.go
@@ -2,6 +2,21 @@ package types
 
 import "time"
 
+type PinningStatus string
+
+const (
+	/*
+	   - queued     # pinning operation is waiting in the queue; additional info can be returned in info[status_details]
+	   - pinning    # pinning in progress; additional info can be returned in info[status_details]
+	   - pinned     # pinned successfully
+	   - failed     # pinning service was unable to finish pinning operation; additional info can be found in info[status_details]
+	*/
+	PinningStatusPinning PinningStatus = "pinning"
+	PinningStatusPinned  PinningStatus = "pinned"
+	PinningStatusFailed  PinningStatus = "failed"
+	PinningStatusQueued  PinningStatus = "queued"
+)
+
 type IpfsPin struct {
 	Cid     string                 `json:"cid"`
 	Name    string                 `json:"name"`
@@ -16,4 +31,18 @@ type IpfsPinStatus struct {
 	Pin       IpfsPin                `json:"pin"`
 	Delegates []string               `json:"delegates"`
 	Info      map[string]interface{} `json:"info"`
+}
+
+type IpfsPinStatusResponse struct {
+	RequestID string                 `json:"requestid"`
+	Status    PinningStatus          `json:"status"`
+	Created   time.Time              `json:"created"`
+	Delegates []string               `json:"delegates"`
+	Info      map[string]interface{} `json:"info"`
+	Pin       IpfsPin                `json:"pin"`
+}
+
+type IpfsListPinStatusResponse struct {
+	Count   int                      `json:"count"`
+	Results []*IpfsPinStatusResponse `json:"results"`
 }
